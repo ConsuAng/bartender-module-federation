5e9a4b4a9d1f4749704b9d2b8bb859f9b26d2914
Consue <c.anguitasalinas@gmail.com>
2024-04-20 22:30:56 -0400

    ✨ mf-user added

---
+++ projects/mf-user/src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [{
+  path:'',
+  loadChildren: () => import('./profile/profile.module').then((m)=> m.ProfileModule)
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
